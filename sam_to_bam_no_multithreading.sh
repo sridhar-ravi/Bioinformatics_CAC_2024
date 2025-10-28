f02f23448c489561ec7da2fb1dc21662b9e462f2
Sridhar Ravichandran <75294130+sridhar-ravi@users.noreply.github.com>
2025-10-28 10:01:10 -0400

    Modify SLURM directives in sam_to_bam_no_multithreading.sh
    Updated SLURM directives for Frontenac 2.0 compatibility.

--- sam_to_bam_no_multithreading.sh
+++ sam_to_bam_no_multithreading.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --account=teaching
-#SBATCH --reservation=teaching
+##SBATCH --account=teaching # not required for Frontenac 2.0
+##SBATCH --reservation=teaching # not required for Frontenac 2.0
 ##SBATCH --ntasks=4               # number of MPI processes
 #SBATCH --cpus-per-task 3
 #SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
 #SBATCH --time=0-3:00           # time (DD-HH:MM)
 #SBATCH --output=%x-%j.out
 
-module load samtools
+module load samtools/1.18
 
 for files in *.sam
 do
