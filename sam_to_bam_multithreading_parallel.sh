d1667336ccfc4ebb7547c6148be23e9d08706ff9
Sridhar Ravichandran <75294130+sridhar-ravi@users.noreply.github.com>
2025-10-28 09:57:17 -0400

    Update SLURM directives and file search pattern

--- sam_to_bam_multithreading_parallel.sh
+++ sam_to_bam_multithreading_parallel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --account=teaching
-#SBATCH --reservation=teaching
+##SBATCH --account=teaching # not required for Frontenac 2.0
+##SBATCH --reservation=teaching # not required for Frontenac 2.0
 ##SBATCH --ntasks=4               # number of MPI processes
-#SBATCH --cpus-per-task 4
+#SBATCH --cpus-per-task 24
 #SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
 #SBATCH --time=0-3:00           # time (DD-HH:MM)
 #SBATCH --output=%x-%j.out
 
 ml samtools
 
-find . -name "*mysample*.sam" | parallel -j 4 "time samtools view -@ 6 -bS {} | samtools sort -o {.}_mt_sorted.bam"
+find . -name "*.sam" | parallel -j 4 "time samtools view -@ 6 -bS {} | samtools sort -o {.}_mt_sorted.bam"
 
 
