95d1d0989d667ddfd886a8123434a756555414eb
Sridhar Ravichandran <75294130+sridhar-ravi@users.noreply.github.com>
2025-10-28 09:54:45 -0400

    Modify SLURM directives and samtools version
    Updated SLURM directives for resource allocation and changed samtools version.

--- sam_to_bam_multithreading.sh
+++ sam_to_bam_multithreading.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --account=teaching
-#SBATCH --reservation=teaching
-#SBATCH --cpus-per-task 2
+##SBATCH --account=teaching #not required for Frontenac 2.0
+##SBATCH --reservation=teaching # not required for Frontenac 2.0
+#SBATCH --cpus-per-task 4
 #SBATCH --mem-per-cpu=4G      # memory; default unit is megabytes
 #SBATCH --time=0-3:00           # time (DD-HH:MM)
 #SBATCH --output=%x-%j.out
 
-ml samtools
+ml samtools/1.18
 
 for files in *.sam
 do
